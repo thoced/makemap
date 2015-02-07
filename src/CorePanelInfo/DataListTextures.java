fae4ec4387649987d82782e44e2f7ff479fb5be2
THOCED <thonon_cedric@hotmail.com>
2015-02-08 00:13:44 +0100

    ajout de la methode comparable et detection du niveau de texture

--- src/CorePanelInfo/DataListTextures.java
+++ src/CorePanelInfo/DataListTextures.java
@@ -2,7 +2,7 @@ package CorePanelInfo;
 
 import java.io.File;
 
-public class DataListTextures 
+public class DataListTextures implements java.lang.Comparable
 {
 	private File file;
 
@@ -42,6 +42,60 @@ public class DataListTextures
 	 */
 	public void setFile(File file) {
 		this.file = file;
+	}
+
+	@Override
+	public int compareTo(Object other) 
+	{
+		int valuePrefix = 0;
+		int valueOther = 0;
+		
+		// TODO Auto-generated method stub
+		DataListTextures data = (DataListTextures) other;
+		String nameOther = data.getFile().getName();
+		String prefixOther = nameOther.substring(0, 3);
+		
+		String nameCurrent = this.getFile().getName();
+		String prefixCurrent = nameCurrent.substring(0, 3);
+		
+		// value pour prefixcurrent
+		switch(prefixCurrent)
+		{
+		case "1EP" :  valuePrefix = 0;break;
+		
+		
+		case "2EP": valuePrefix = 1; break;
+		
+			
+		case "3EP": valuePrefix = 2; break;
+		
+		case "4EP": valuePrefix = 3; break;
+		
+		case "5EP" : valuePrefix = 4; break;
+		}
+		
+		switch(prefixOther)
+		{
+		case "1EP" :  valueOther = 0;break;
+		
+		
+		case "2EP": valueOther = 1; break;
+		
+			
+		case "3EP": valueOther = 2; break;
+		
+		case "4EP": valueOther = 3; break;
+		
+		case "5EP" : valueOther = 4; break;
+		}
+		
+		if(valueOther > valuePrefix)
+			return -1;
+		else if(valueOther == valuePrefix)
+			return 0;
+		else 
+			return 1;
+			
 	}
 
 	
