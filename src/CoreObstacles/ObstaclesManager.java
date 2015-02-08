1bcf9766d4a0da77763d8032bcb3f07295c354e4
THOCED <thonon_cedric@hotmail.com>
2015-02-08 09:43:14 +0100

    création du currentObstacle et ajout du MVC

--- src/CoreObstacles/ObstaclesManager.java
+++ src/CoreObstacles/ObstaclesManager.java
@@ -11,8 +11,24 @@ public class ObstaclesManager implements Drawable
 {
 	private List<Obstacle> listObstacles;
 	
+	// list du MVC attach
+	private static List<IObstacleMVC> listMVC = new ArrayList();
+	
+	// parent
+	private static ObstaclesManager parent;
+	
+	// cpt des noms
+	private int nameCpt = 0;
+	
+	// currentObstacle selectionné
+	private static Obstacle currentObstacle = null;
+	
+	
+	
 	public ObstaclesManager()
 	{
+		// parent
+		parent = this;
 		// instance
 		listObstacles = new ArrayList<Obstacle>();
 		
@@ -32,9 +48,51 @@ public class ObstaclesManager implements Drawable
 	{
 		// création d'un nouvelle objet obstacle
 		Obstacle o = new Obstacle();
+		// on specifie le currentObstacle
+		this.currentObstacle = o;
+		// créatin du nom de l'obstacle
+		o.setName("obstacle " + String.valueOf(nameCpt));
+		nameCpt++;
 		// ajout de l'obstacle dans la liste des obstacles
 		listObstacles.add(o);
+		// update des mvc
+		this.refreshMVC();
 		// retour de l'obstalce
+		
 		return o;
 	}
+	
+	public void refreshMVC()
+	{
+		// rappel des mvc
+		for(IObstacleMVC o : this.listMVC)
+		{
+			o.updateMVC(this.listObstacles);
+		}
+			
+	}
+	
+	public static void attachMVC(IObstacleMVC obj)
+	{
+		// attach un objet mvc
+		parent.listMVC.add(obj);
+	}
+
+	/**
+	 * @return the currentObstacle
+	 */
+	public static Obstacle getCurrentObstacle() {
+		return currentObstacle;
+	}
+
+	/**
+	 * @param currentObstacle the currentObstacle to set
+	 */
+	public static void setCurrentObstacle(Obstacle currentObstacle) {
+		ObstaclesManager.currentObstacle = currentObstacle;
+	}
+
+	
+	
+	
 }
