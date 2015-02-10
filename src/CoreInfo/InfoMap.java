32a978fc06f13d267d0c77980b7035b3891ec03a
THOCED <thonon_cedric@hotmail.com>
2015-02-10 19:43:09 +0100

    Création de la class InfoMap utilisable pour les informations de la map

---
+++ src/CoreInfo/InfoMap.java
@@ -0,0 +1,43 @@
+package CoreInfo;
+
+public class InfoMap 
+{
+	private int widthMap;
+	
+	private int heightMap;
+
+	/**
+	 * @return the widthMap
+	 */
+	public int getWidthMap() {
+		return widthMap;
+	}
+
+	/**
+	 * @param widthMap the widthMap to set
+	 */
+	public void setWidthMap(int widthMap) {
+		this.widthMap = widthMap;
+	}
+
+	/**
+	 * @return the heightMap
+	 */
+	public int getHeightMap() {
+		return heightMap;
+	}
+
+	/**
+	 * @param heightMap the heightMap to set
+	 */
+	public void setHeightMap(int heightMap) {
+		this.heightMap = heightMap;
+	}
+
+	/**
+	 * @return the width_map
+	 */
+	
+	
+	
+}
