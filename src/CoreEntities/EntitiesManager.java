920e054bda8bbb0a9dededad01909018d72f0126
THOCED <thonon_cedric@hotmail.com>
2015-02-17 15:35:35 +0100

    Ajout de la liste des MVC

--- src/CoreEntities/EntitiesManager.java
+++ src/CoreEntities/EntitiesManager.java
@@ -3,14 +3,26 @@ package CoreEntities;
 import java.util.ArrayList;
 import java.util.List;
 
+import CoreCalques.ICalqueMVC;
+
 public class EntitiesManager 
 {
 	// liste des entities
 	private List<EntitiesBase> listEntities;
+	// liste des mvc
+	private List<IEntitiesMVC> listMVC;
 	
 	public EntitiesManager()
 	{
 		// instance de la liste des entities
 		this.listEntities = new ArrayList<EntitiesBase>();
+		// instance de la liste des mvc
+		this.listMVC = new ArrayList<IEntitiesMVC>();
+	}
+	
+	public  void attachMVC(IEntitiesMVC mvc)
+	{
+		// ajout dans la liste mvc
+		this.listMVC.add(mvc);
 	}
 }
