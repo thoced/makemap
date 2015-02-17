cdd9491065a4d2b9047d9ecbc8d2d31458780425
THOCED <thonon_cedric@hotmail.com>
2015-02-17 15:36:14 +0100

    Création de la class JListEntities

---
+++ src/CorePanelInfo/JListEntities.java
@@ -0,0 +1,20 @@
+package CorePanelInfo;
+
+import java.util.List;
+
+import javax.swing.JList;
+
+import CoreEntities.EntitiesBase;
+import CoreEntities.IEntitiesMVC;
+
+public class JListEntities extends JList implements IEntitiesMVC
+{
+
+	@Override
+	public void updateEntitiesMVC(List<EntitiesBase> list) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
